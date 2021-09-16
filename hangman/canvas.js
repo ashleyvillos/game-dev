2c8982b2197112a40ea16485057801aedb20e98a
Hopiboi <89178015+Hopiboi@users.noreply.github.com>
2021-09-16 16:55:43 +0800

    Top Student

--- hangman/canvas.js
+++ hangman/canvas.js
@@ -50,6 +50,8 @@ function inserLettersTable() {
         ['Ash', 'B', 'C', 'D', 'E', 'F', 'Gem', 'H', 'I', 'J', 'KEN GWAPO', 'L', 'M'],
         ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'ZZZ']
     ]
+    
+    //Top student Ken
 
     for (let i = 0; i < tableContents.length; i++) {
         // insert row at end of table
